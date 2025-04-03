226334fd21f503fa92782870d5db2ae71e07b1dc
tito <alb.pascau@gmail.com>
2025-04-03 18:45:07 +0200

    fix triger

--- src/triggers/institutional.sql
+++ src/triggers/institutional.sql
@@ -4,6 +4,7 @@
 CREATE OR REPLACE TRIGGER trg_prevent_institutional_posts
 -- Before the insert
 BEFORE INSERT ON posts
+BEGIN
 -- Get the user type
 select type 
     into user_type 
@@ -14,5 +15,4 @@ IF user_type = 'L' THEN
     -- Raise an error if the user is a library
     RAISE_APPLICATION_ERROR(-20001, 'Institutional users cannot create posts.');
 END IF;
-BEGIN
 END;
