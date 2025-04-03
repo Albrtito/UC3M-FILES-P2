f066efd1405abb9ba40286868eb56d504a3fbf20
tito <alb.pascau@gmail.com>
2025-04-03 19:32:07 +0200

    fix trigger institutional

--- src/triggers/institutional.sql
+++ src/triggers/institutional.sql
@@ -1,13 +1,13 @@
 -- Description: Prevents the insertion of posts of institutional users (municipal libraries). The trigger is used before the insertion of a new post in the posttable, checking the type of user by the userID provided for the insert.
 -- If the user.type is 'L' then it is a library
-
 CREATE OR REPLACE TRIGGER trg_prevent_institutional_posts
 -- Before the insert
 BEFORE INSERT ON posts
 FOR EACH ROW
 DECLARE
-user_type users.type%TYPE;
+    user_type users.type%TYPE;
 BEGIN
+
 -- Get the user type
     select type into user_type 
         from users 
