5d4ad6002ba5d80dffd915474c6e3eaa8366925b
tito <alb.pascau@gmail.com>
2025-04-04 14:37:32 +0200

    add the subquery

--- src/queries/partial.sql
+++ src/queries/partial.sql
@@ -1,5 +1,27 @@
 -- Get the stops per active year for each driver
+WITH 
+-- Get the info that we need from the drivers table into an smaller table 
+-- passport (so that we can still identify), Full name, birthdate age(needs to be computed), seniority(needs to be computed), active years(needs to be computed), we also maintain the contract start and end dates
+driver_base AS (
+    SELECT 
+        d.passport,
+        d.fullname,
+        d.birthdate,
+        d.cont_start,
+        d.cont_end,
+        -- Calculate age in years
+        TRUNC(MONTHS_BETWEEN(SYSDATE, d.birthdate) / 12) AS age,
+        -- Calculate seniority (contract length) in years. 
+        -- Use NVL to handle NULL end dates, if no end date, then use SYSDATE
+        TRUNC(MONTHS_BETWEEN(NVL(d.cont_end, SYSDATE), d.cont_start) / 12) AS seniority_years,
+        -- Dont get what's the difference between this and the one above, so add it here as the same thing
+        TRUNC(MONTHS_BETWEEN(NVL(d.cont_end, SYSDATE), d.cont_start) / 12) AS active_years
+    FROM 
+        drivers d
+)
     SELECT
+    WITH driver_stops AS (
+
         ds.passport,
         db.fullname,
         db.active_years,
